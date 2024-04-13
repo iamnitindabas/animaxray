989d761b556f5983685c3e4f1cf94caef7b102d0
iamnitindabas <nitindabas1000@gmail.com>
2024-04-14 03:09:49 +0530

    small change

--- src/Components/AnimeList.tsx
+++ src/Components/AnimeList.tsx
@@ -53,7 +53,7 @@ const AnimeList: React.FC<AnimeListProps> = ({ singleAnimeData }) => {
           </div>
 
           <div className="grid grid-cols-[100%] grid-rows-[auto_50px] gap-1.5 max-h-[320px]">
-            <div className="p-5 overflow-auto transition-all  text-[#5c728a]  dark:text-[#9fadbd]">
+            <div className="p-5 overflow-auto transition-all  text-[#495b6e]  dark:text-[#9fadbd]">
               <div className="grid grid-cols-2">
                 <div>
                   <p className="text-xs font-bold">
@@ -63,17 +63,24 @@ const AnimeList: React.FC<AnimeListProps> = ({ singleAnimeData }) => {
                       : "N/A"}
                   </p>
                   <p className="text-tiny font-bold">
-                    Duration: {singleAnimeData.duration}
+                    Duration:{" "}
+                    {singleAnimeData.duration
+                      ? singleAnimeData.duration
+                      : "N/A"}
                   </p>
                 </div>
                 <div className="flex items-center justify-end gap-4">
                   <div className="flex items-center justify-between ">
                     <img src={heart} alt="" className="size-7 pr-2" />#
-                    {singleAnimeData.popularity}
+                    {singleAnimeData.popularity
+                      ? singleAnimeData.popularity
+                      : "N/A"}
                   </div>
                   <div className="flex items-center justify-evenly">
                     <img src={star} alt="" className="size-7 pr-2" />
-                    <p>{singleAnimeData.score}</p>
+                    <p>
+                      {singleAnimeData.score ? singleAnimeData.score : "N/A"}
+                    </p>
                   </div>
                 </div>
               </div>
