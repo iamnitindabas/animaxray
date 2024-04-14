023cb1fa581192e5da3f81d113515540f736cdb1
Harsh Dabas <iamharshdabas@gmail.com>
2024-04-14 16:22:19 +0530

    chore: light more ? we don't offer that here.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 const App: React.FC = () => {
   return (
     <NextUIProvider>
+      {/* NOTE: light mode ? seriously ? */}
       <main className="dark">
         {/* <header className="w-full fixed top-0 h-[300px] bg-[#2b2d42]"></header> */}
         <div className="bg-[#d6e0ef] dark:bg-[#13171d] bg-center bg-no-repeat bg-cover ">
